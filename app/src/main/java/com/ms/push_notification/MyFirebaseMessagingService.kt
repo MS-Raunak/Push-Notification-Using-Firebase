33c71b3772329518f63379cf8a5238e2dfb8ca88
MS-Raunak <raunakkr.saha@gmail.com>
2023-04-24 17:53:59 +0530

    push notification using firebase

---
+++ app/src/main/java/com/ms/push_notification/MyFirebaseMessagingService.kt
@@ -0,0 +1,68 @@
+package com.ms.push_notification
+
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_MUTABLE
+import android.app.PendingIntent.FLAG_ONE_SHOT
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+const val channelId = "notification_channel"
+const val channelName = "com.ms.push_notification"
+@SuppressLint("MissingFirebaseInstanceTokenRefresh")
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    // generate the notification
+    // attach the notification created with the custom layout
+    // show the notification
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+       if (remoteMessage.notification != null){
+           generateNotification(remoteMessage.notification!!.title!!, remoteMessage.notification!!.body!!)
+       }
+    }
+
+    fun getRemoteView(title : String, message: String) : RemoteViews{
+        val remoteViews = RemoteViews("com.ms.push_notification", R.layout.notification)
+        remoteViews.setTextViewText(R.id.title, title)
+        remoteViews.setTextViewText(R.id.message, message)
+        remoteViews.setImageViewResource(R.id.app_logo, R.drawable.gfg)
+        return remoteViews
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    fun generateNotification(tile:String, message: String){
+        val intent = Intent(this,MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val pendingIntent = PendingIntent.getActivity(this,0,intent, FLAG_MUTABLE)
+        // channel id, name
+        var builder : NotificationCompat.Builder = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.gfg)
+            .setAutoCancel(true)
+            .setVibrate(longArrayOf(1000, 1000, 1000, 1000))
+            .setOnlyAlertOnce(true)
+            .setContentIntent(pendingIntent)
+        builder = builder.setContent(getRemoteView(tile, message))
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S){
+            val notificationChannel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        notificationManager.notify(0, builder.build())
+
+    }
+
+}
